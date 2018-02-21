72a1810a9ae41336ba440a1bed5e464d4201dde4
Faelwen <francis.john.mccarthy@googlemail.com>
2018-02-22 00:04:06 +0100

    Add project files.

---
+++ StarfinderSpellParsing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StarfinderSpellParsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
