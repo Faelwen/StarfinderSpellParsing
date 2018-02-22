85382f99aa3d3467109b0129f88bd97a9ebf1fbd
Faelwen <francis.john.mccarthy@googlemail.com>
2018-02-22 23:51:35 +0100

    Compare csv & xml

--- StarfinderSpellParsing/Program.cs
+++ StarfinderSpellParsing/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml;
+using System.Text;
 
 namespace StarfinderSpellParsing
 {
@@ -15,8 +16,9 @@ namespace StarfinderSpellParsing
             var spellModuleXML = directory + "\\common.xml";
             var spellShortDescriptionTSV = directory + "\\SpellShortDescription.tsv";
 
-            using (var spellModuleStream = new StreamReader(spellModuleXML))
+
             using (var spellShortDescriptionStream = new StreamReader(spellShortDescriptionTSV))
+            using (var spellModuleStream = new StreamReader(spellModuleXML, Encoding.GetEncoding(28591)))
             {
                 Console.WriteLine("Files opened");
 
@@ -24,11 +26,25 @@ namespace StarfinderSpellParsing
                 spellShortDescriptionCSVreader.Configuration.Delimiter = "\t";
                 spellShortDescriptionCSVreader.Configuration.HasHeaderRecord = false;
                 var records = spellShortDescriptionCSVreader.GetRecords<SpellShortDescription>();
-
+                var recordList = new List<SpellShortDescription>();
+                foreach(var record in records) { recordList.Add(record); }
                 var spellModuleXmlDocument = new XmlDocument();
                 spellModuleXmlDocument.Load(spellModuleStream);
 
-                Console.WriteLine(GetSpellShortDescriptionRecord("Mind Thrust I", records).Name);
+                //Get spells from xml file
+                var root = spellModuleXmlDocument.DocumentElement;
+                var spellrefnode = root.SelectNodes("/root/reference/spells/category/*");
+
+                //Cross ref with the csv file
+                foreach(XmlNode spell in spellrefnode)
+                {
+                    var spellName = spell["name"].InnerText;
+                    var shortDescriptionRecord = GetSpellShortDescriptionRecord(spellName, recordList);
+                    if (shortDescriptionRecord == null)
+                    {
+                        Console.WriteLine("{0} not found!", spellName);
+                    }
+                }
 
                 spellShortDescriptionCSVreader.Dispose();
             }
@@ -37,7 +53,7 @@ namespace StarfinderSpellParsing
             Console.ReadKey();
         }
 
-        private static SpellShortDescription GetSpellShortDescriptionRecord(String spellname, IEnumerable<SpellShortDescription> records)
+        private static SpellShortDescription GetSpellShortDescriptionRecord(String spellname, List<SpellShortDescription> records)
         {
             var isFound = false;
             var record = new SpellShortDescription();
